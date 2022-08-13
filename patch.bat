d45683afc8b00096813be69ac35c04d7891645a6
PeaceShi <peaceshi@outlook.com>
2022-08-13 23:17:21 +0800

    Add files via upload

---
+++ patch.bat
@@ -0,0 +1,12 @@
+.\hpatchz.exe .\Script\Script.JBF .\diff\Script.JBF.diff .\Script\Script.JBF.new
+.\hpatchz.exe .\Texture\Texture.JBF .\diff\Texture.JBF.diff .\Texture\Texture.JBF.new
+.\hpatchz.exe .\Wind3Data\Wind3Data.JBF .\diff\Wind3Data.JBF.diff .\Wind3Data\Wind3Data.JBF.new
+
+move .\Script\Script.JBF .\Script\Script.JBF.steam
+move .\Script\Script.JBF.new .\Script\Script.JBF
+
+move .\Texture\Texture.JBF .\Texture\Texture.JBF.steam
+move .\Texture\Texture.JBF.new .\Texture\Texture.JBF
+
+move .\Wind3Data\Wind3Data.JBF .\Wind3Data\Wind3Data.JBF.steam
+move .\Wind3Data\Wind3Data.JBF.new .\Wind3Data\Wind3Data.JBF
