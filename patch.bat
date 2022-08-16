a2199da64fc8e4edae13129c3521f5cf5ee053ee
PeaceShi <peaceshi@outlook.com>
2022-08-16 14:48:46 +0800

    Add files via upload

--- patch.bat
+++ patch.bat
@@ -3,7 +3,8 @@
 .\hpatchz.exe .\Wind3Data\Wind3Data.JBF .\diff\Wind3Data.JBF.diff .\Wind3Data\Wind3Data.JBF.new
 .\hpatchz.exe .\protect.dll .\diff\protect.dll.diff .\protect.dll.new
 .\hpatchz.exe .\WIND3.EXE .\diff\WIND3.EXE.diff .\WIND3.EXE.new
-.\hpatchz.exe .\Template\TemplateData\FixedData_Tool.DATA .\diff\FixedData_Tool.DATA .\Template\TemplateData\FixedData_Tool.DATA.new
+.\hpatchz.exe .\Template\TemplateData\FixedData_Tool.DATA .\diff\FixedData_Tool.DATA.diff .\Template\TemplateData\FixedData_Tool.DATA.new
+.\hpatchz.exe .\WindConfig.exe .\diff\WindConfig.exe.diff .\WindConfig.exe.new
 
 del .\Script\Script.JBF.steam
 move .\Script\Script.JBF .\Script\Script.JBF.steam
@@ -24,6 +25,11 @@ move .\protect.dll.new .\protect.dll
 del .\WIND3.EXE.steam
 move .\WIND3.EXE .\WIND3.EXE.steam
 move .\WIND3.EXE.new .\WIND3.EXE
+
+
+del .\WindConfig.exe.steam
+move .\WindConfig.exe .\WindConfig.exe.steam
+move .\WindConfig.exe.new .\WindConfig.exe
 
 del .\Template\TemplateData\FixedData_Tool.DATA.steam
 move .\Template\TemplateData\FixedData_Tool.DATA .\Template\TemplateData\FixedData_Tool.DATA.steam
